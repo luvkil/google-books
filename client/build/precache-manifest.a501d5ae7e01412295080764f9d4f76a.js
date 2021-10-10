1288b2d530d3254db9d9e1ce14527c97e0522b18
Aldaine Knight <stevennite1@gmail.com>
2021-10-09 21:16:46 -0400

    update package.json

---
+++ client/build/precache-manifest.a501d5ae7e01412295080764f9d4f76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27412a1bf14206bba5e8",
+    "url": "/static/css/main.d5755d2a.chunk.css"
+  },
+  {
+    "revision": "27412a1bf14206bba5e8",
+    "url": "/static/js/main.1ee3911a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e94221c3e99a3ee361c",
+    "url": "/static/js/2.463ba994.chunk.js"
+  },
+  {
+    "revision": "eab5f559f956f9058512480fc0c50af6",
+    "url": "/static/media/header-blueBurst.eab5f559.jpg"
+  },
+  {
+    "revision": "6533a20945c99f87fc82e192225ea3ec",
+    "url": "/index.html"
+  }
+];
